26478dcae3b446c12aeef2af2bb203968d896e6d
Tianjiao Huang <maitian519@gmail.com>
2019-07-16 21:17:03 -0700

    format program options

--- buffer/circular_buffer.cpp
+++ buffer/circular_buffer.cpp
@@ -1,9 +1,6 @@
 // Producer produces `num_data` amount of numbers and put them into the buffer
 // one after another. Consumer consumes those numbers and output their sum.
 
-// Questions:
-// 1. Let's say n=1, k1=3, k2=2, k3=2, and both P and C are ready to work at
-// t=0. Basically sleep_for or sleep_until
 #include "circular_buffer.hpp"
 
 #include <cassert>
@@ -76,26 +73,29 @@ void consumer_fn(Buffer *buffer, uint64_t *result, int k3) {
   }
 }
 
-// arg1: n, size of the circular array
-// arg2: k1, P produces a burst of k1 items, 1 per time unit.
-// arg3: k2, Then P waits for k2 time units until the next burst of k1 item.
-// arg4: k3, C removes 1 item every k3 time units.
 int main(int argc, char **argv) {
   // Parse program arguments
   int n, k1, k2, k3;
   po::options_description desc("Program options");
-  desc.add_options()("help", "produce help message")(
-      "n", po::value<int>(&n)->required(), "capacity of the buffer")(
-      "k1", po::value<int>(&k1)->required(),
-      "P burst length")("k2", po::value<int>(&k2)->required(), "P rest length")(
-      "k3", po::value<int>(&k3)->required(), "time to comsum 1 entry for C");
+  desc.add_options()("help", "list all flags");
+  desc.add_options()("verbose", "produce help message");
+  desc.add_options()("n", po::value<int>(&n)->required(),
+                     "capacity of the buffer");
+  desc.add_options()("k1", po::value<int>(&k1)->required(),
+                     "P produces a burst of k1 items, 1 per time unit.");
+  desc.add_options()("k2", po::value<int>(&k2)->required(),
+                     "After producing an item, P waits for k2 time units until "
+                     "the next burst of k1 item.");
+  desc.add_options()("k3", po::value<int>(&k3)->required(),
+                     "C removes 1 item every k3 time units.");
   po::variables_map vm;
-  assert(vm.count("n"));
   po::store(po::parse_command_line(argc, argv, desc), vm);
+
   if (vm.count("help")) {
     std::cout << desc << std::endl;
     return 0;
   }
+  assert(vm.count("n"));
 
   Buffer buffer(n);
   uint64_t result = 0;
