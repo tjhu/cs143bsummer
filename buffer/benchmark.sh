d29f54d15cee31d18217ea2b9465119dc7b6024e
Tianjiao Huang <maitian519@gmail.com>
2019-07-24 23:19:16 -0700

    bash exit on interrupt

--- buffer/benchmark.sh
+++ buffer/benchmark.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+trap 'exit 130' INT
+
 function run() {
     /usr/bin/time -f "time: %e\n" ./circular_buffer_main $1 $2 $3 $4 $5 $6
 }
@@ -5,6 +8,6 @@ function run() {
 for i in {1..20..1}
   do 
     echo "iteration $i"
-    run $i 10 10 2 >&1
+    run $i 10 10 2 2>&1
     echo
  done
